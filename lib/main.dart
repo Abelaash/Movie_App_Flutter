a252ee0f5518423525a710db5335444b6fbf0d5d
abela <abelaash.giritharan@gmail.com>
2020-08-28 10:24:24 -0400

    add all files to repo.

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'file:///C:/Users/abela/IdeaProjects/movie_app/lib/ui/home.dart';
+
+void main() => runApp(new MaterialApp(
+  home: MovieListView(),
+  debugShowCheckedModeBanner: false,
+
+));
